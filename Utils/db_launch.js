00b31c256fee7ee514833a3bca5425e0809ee0fc
Ilya <geil1997@mail.ru>
2025-03-19 23:41:25 +0300

    Вывод отладочных сообщений

--- Utils/db_launch.js
+++ Utils/db_launch.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config({ path: `./env/.env.${process.env.NODE_ENV}` });
 
+console.log("DB Config:", {
+  host: process.env.DB_HOST,
+  name: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD ? "HIDDEN" : "MISSING",
+});
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
