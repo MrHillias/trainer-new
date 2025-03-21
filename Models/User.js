2ec75b1cd06a6cb76f8ae497397d6174d045c697
Ilya <geil1997@mail.ru>
2025-03-21 15:26:07 +0300

    Апдейт модели с использованием uuid и добавление скрпитов для отката миграций

--- Models/User.js
+++ Models/User.js
@@ -1,36 +1,33 @@
-const { DataTypes, Model } = require("sequelize");
-const bcrypt = require("bcrypt");
+const { DataTypes } = require("sequelize");
 const sequelize = require("../Utils/db_launch");
 
-class User extends Model {
-  async validPassword(password) {
-    return await bcrypt.compare(password, this.password);
-  }
-}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    name: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false, unique: true },
-    password: { type: DataTypes.STRING, allowNull: false },
-    isConfirmed: { type: DataTypes.BOOLEAN, defaultValue: false },
-    confirmationToken: { type: DataTypes.STRING, allowNull: true },
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
   },
-  {
-    sequelize,
-    modelName: "User",
-    hooks: {
-      beforeCreate: async (user) => {
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, salt);
-      },
-    },
-  }
-);
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isConfirmed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  confirmationToken: {
+    type: DataTypes.UUID, // Должен быть UUID
+    defaultValue: DataTypes.UUIDV4, // Генерирует случайный UUID
+  },
+});
 
 module.exports = User;
--- migrations/20250321112033-add-confirmation-token-to-users.js
+++ migrations/20250321112033-add-confirmation-token-to-users.js
@@ -4,8 +4,9 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn("Users", "confirmationToken", {
-      type: Sequelize.STRING,
-      allowNull: true,
+      type: Sequelize.UUID,
+      allowNull: false,
+      defaultValue: Sequelize.literal("uuid_generate_v4()"), // Генерация UUID
     });
   },
 
