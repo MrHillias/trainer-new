a7945aae9b3b42eb9d836d80485a60a14ca69cfb
Ilya <geil1997@mail.ru>
2025-03-21 18:46:42 +0300

    export fix

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
