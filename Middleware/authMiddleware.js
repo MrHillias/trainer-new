f9e768f3ff21ffa8506f8c76d5aad1e0aa158d05
Ilya <geil1997@mail.ru>
2025-03-21 18:43:38 +0300

    fix middleware

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -1,19 +1,20 @@
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {
-  const token = req.header("Authorization")?.split(" ")[1];
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
-    return res.status(401).json({ error: "Доступ запрещен. Токен не найден." });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Отсутствует токен авторизации" });
   }
+
+  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    req.user = decoded; // Добавляем данные пользователя в объект запроса
     next();
   } catch (error) {
-    res.status(401).json({ error: "Неверный или просроченный токен." });
+    return res.status(403).json({ error: "Неверный или истекший токен" });
   }
 };
-
 module.exports = authMiddleware;
